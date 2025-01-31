c78fa8cce2e460673ce86e039d5ce6db890b08a4
ez4bruce <ez4bruce@gmail.com>
2025-01-31 13:52:02 +0800

    update

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -1,7 +1,7 @@
 GOPATH=$(go env GOPATH)
 mkdir temp && cd temp
-wget https://github.com/protocolbuffers/protobuf/releases/download/v26.0/protoc-26.0-linux-x86_64.zip
-unzip protoc-26.0-linux-x86_64.zip
+wget https://github.com/protocolbuffers/protobuf/releases/download/v29.3/protoc-29.3-linux-x86_64.zip
+unzip protoc-29.3-linux-x86_64.zip
 
 mv bin/protoc $GOPATH/bin
 cd ../
